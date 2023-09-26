f66b9e2c862d53de51d8fecc97c11edfdc10da51
jailbreakerVC <54243183+jailbreakerVC@users.noreply.github.com>
2023-09-26 21:42:37 +0530

    hardcode amity

--- src/App.jsx
+++ src/App.jsx
@@ -11,51 +11,61 @@ function App() {
   const [safeToLoad, setSafeToLoad] = useState(false);
   const [pointers, setPointers] = useState([]);
 
+  // useEffect(() => {
+  //   async function getpoints() {
+  //     let { data: points, error } = await supabase.from("points").select("*");
+  //     console.log("Data: ", points);
+  //     setPointers(points);
+  //   }
+  //   function success(position) {
+  //     const latitude = position.coords.latitude;
+  //     const longitude = position.coords.longitude;
+  //     setCurrentLat(latitude);
+  //     setCurrentLon(longitude);
+  //     setSafeToLoad(true);
+  //     getpoints();
+  //   }
+  //   function error() {
+  //     console.log("Unable to retrieve your location");
+  //     alert("Cannot access location", error);
+  //   }
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(success, error);
+  //   } else {
+  //     console.log("Geolocation not supported");
+  //     return;
+  //   }
+  // }, []);
+
   useEffect(() => {
     async function getpoints() {
       let { data: points, error } = await supabase.from("points").select("*");
       console.log("Data: ", points);
       setPointers(points);
     }
-    function success(position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-      setCurrentLat(latitude);
-      setCurrentLon(longitude);
-      setSafeToLoad(true);
-      getpoints();
-    }
-    function error() {
-      console.log("Unable to retrieve your location");
-      alert("Cannot access location", error);
-    }
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, error);
-    } else {
-      console.log("Geolocation not supported");
-      return;
-    }
+    getpoints();
   }, []);
-  if (!safeToLoad) {
-    return (
-      <div>
-        <p>loading map</p>
-      </div>
-    );
-  } else {
-    return (
-      <>
-        <div id="map">
-          <MapContainer
-            center={[currentLat, currentLon]}
-            zoom={100}
-            scrollWheelZoom={true}
-          >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {/* <Marker position={position}>
+
+  // if (!safeToLoad) {
+  //   return (
+  //     <div>
+  //       <p>loading map</p>
+  //     </div>
+  //   );
+  // } else {
+  return (
+    <>
+      <div id="map">
+        <MapContainer
+          center={[28.5439, 77.3331]}
+          zoom={100}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {/* <Marker position={position}>
               <Popup>
                 <img
                   src="src/assets/opendrain.jpeg"
@@ -65,16 +75,15 @@ function App() {
                 <p>Open drain</p>
               </Popup>
             </Marker> */}
-            {pointers.map
-              ? pointers.map((pointer) => {
-                  return <Pointer data={pointer} />;
-                })
-              : null}
-          </MapContainer>
-        </div>
-      </>
-    );
-  }
+          {pointers.map
+            ? pointers.map((pointer) => {
+                return <Pointer data={pointer} />;
+              })
+            : null}
+        </MapContainer>
+      </div>
+    </>
+  );
 }
 
 export default App;
